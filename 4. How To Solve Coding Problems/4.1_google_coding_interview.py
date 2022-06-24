5f4ada4d935295b8df2868d44627a4aeaaef62b5
Daniel Chukwu <00danzy@gmail.com>
2022-06-24 22:26:57 +0100

    google coding interview 19mins in

--- 4. How To Solve Coding Problems/4.1_google_coding_interview.py
+++ 4. How To Solve Coding Problems/4.1_google_coding_interview.py
@@ -29,7 +29,7 @@
 # Ans: we could start with the first element and brinary search the rest of the array for a match and then binary search the next element then binary search the rest for a match and so on
 # Interviewer - True, Valid answer but that would still be too costly since you are doing a log n for finding and an n for each iteration --> we would be left with an O(n log n) and that's still too costly
 
-# ANSWER - 3rd Tru
+# ANSWER - 3rd Tru+
 # Okay Let me think - How about at the start of the problem i use 2 pointers(pointing to start and end)
 # e.g [1, 2, 3, 9]
 #      ^        ^
@@ -69,3 +69,65 @@
 
 # You - I just remembered i don't know how i am supposed to return this. Should it be a yes i found it or the indexes of the pair or the actual pairs
 # Interviewer - I would prefer you return if you found it or not. But let's say you were going to return a pair, how could that be a problem returning a pair if there wasn't no pair
+# Ans - well i guess i could return a data structure that holds a boolean found; and a pair if any
+# e.g - { found: false, pair: ""}
+# Interviewer - Well rather than going with a custom object how about you just return a Boolean
+
+
+# Okay Let's get coding in Python 🏄‍♂️🏄‍♂️🏄‍♂️
+
+def hasPairWithSum(collection, sum):
+   low = 0
+   hi = len(collection) - 1
+   while low < hi:
+      added = collection[low] + collection[hi]
+
+      if added == sum:
+         return True
+      elif added > sum:
+         hi -= 1
+         continue
+      elif added < sum:
+         low +=1
+         continue
+   return False
+
+result = hasPairWithSum([1, 2, 3, 9], 8) # O(n)
+# print(f"Pair Found: {result}")
+
+
+# Interviewer - Excellent answer✅, but i'd like to throw a little runch in there. What if the collections are no longer sorted. How would you then solve this problem to achieve an O(n) Big O notation
+# Ans: Well first off in order to solve this problem the same way with our above O(n) Algorithm we would have to sort it at the top of the function. Which would be an O(n log n) Time Complexity
+# Interviewer - well that's too slow, Google needs something faster ** she giggles
+
+# ANSWER - 4th
+# Ans - Okay, well since that won't work i am thinking iterating through each item in the collection and then adding the compliment of each item --> As i progress in the iteration, i am asking current number, is this current number in our compliments (using a hash table data structure for lookup O(1) and insertion O(1)) --> if so we've seen a previous number whose compliment adds up to == sum --> return True else: add current number to compliments and move to the next iteration
+# e.g [1⁷, 2, 3, 9]  --> is 1 a compliment of any value seen before | compliments = {} | answer = No
+#      ^
+#     compliment = {7}
+#     [1⁷, 2⁶, 3, 9]  --> is 2 a compliment of any value seen before | compliments = {7} | answer = No
+#          ^
+#     compliment = {7, 6}
+#     [1⁷, 2⁶, 3⁵, 9]  --> is 3 a compliment of any value seen before | compliments = {7, 6} | answer = No
+#              ^
+#     compliment = {7, 6, 5}
+#     [1⁷, 2⁶, 3⁵, 9¯¹]  --> is 9 a compliment of any value seen before | compliments = {7, 6, 5} | answer = No
+#                   ^
+#     compliment = {7, 6, 5, -1}
+
+# Interviewer - That sounds great to me. how about we get to coding
+# e.g collection1 = [1, 2, 3, 9]
+# e.g collection2 = [1, 2, 4, 4]
+def hasPairWithSum2(collection, sum):
+   compliments = set() # set add and in operations use an underlying hash table as it's data structure -- and hash tables have an insertion and access of O(1) Constant Time
+   for value in collection:
+      if value in compliments: # if current value is a compliment to a value we've seen before
+         return True
+      
+      compliments.add(sum - value)
+   print(compliments)
+   return False
+
+result2 = hasPairWithSum2([1, 2, 4, 9], 8) # O(n) 
+print(result2)
+
