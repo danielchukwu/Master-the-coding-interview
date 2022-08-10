2b1e223508a37f9e35b07d2099f97cb5e399fd24
Daniel Chukwu <00danzy@gmail.com>
2022-08-10 20:21:09 +0100

    solve problem

--- practice.js
+++ practice.js
@@ -1,47 +1,98 @@
 // Javascript - {}
-// Topic - Leetcode (contains duplicate)
-// Problem: Given an integer array nums, return true if any value appears at least twice in the array, and return 
-// false if every element is distinct.
-
+// Topic - Leetcode (Rotate Array)
+// Problem: Given an array, rotate the array to the right by k steps, where k is non-negative
 // Example 1:
 
-// Input: nums = [1,2,3,1]
-// Output: true
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
 // Example 2:
 
-// Input: nums = [1,2,3,4]
-// Output: false
-// Example 3:
-
-// Input: nums = [1,1,1,3,3,4,3,2,4,2]
-// Output: true
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
 
 // Constraints:
 
 // 1 <= nums.length <= 105
-// -109 <= nums[i] <= 109
-// Accepted
-// 1,907,335
-// Submission
+// -231 <= nums[i] <= 231 - 1
+// 0 <= k <= 105
 
+
+// Follow up:
+
+// Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+// Could you do it in-place with O(1) extra space?
 
 
 // SOLUTION 1
-var containsDuplicate = function(nums) {
-   const seenSet = new Set();
-   for (let i = 0; i < nums.length; i++) {
-      if (seenSet.has(nums[i])){
-         console.log(true);
-         return true;
-      }
-      seenSet.add(nums[i]);
-   }
-   console.log(false);
-   return false;
+// using the the k steps we can slice(-k) items from the list and add to slice(0, -k)
+var rotate = function(nums, k) {
+   return [...nums.slice(-k), ...nums.slice(0, -k)];
 };
 
-const t1 = [1,2,3,1];
-const t2 = [1,2,3,4];
-const t3 = [1,1,1,3,3,4,3,2,4,2];
+const t1 = [[1,2,3,4,5,6,7], 3];
+const t2 = [[-1,-100,3,99], 2];
+const t3 = [[-1], 2];
+const t4 = [[1,2], 3];
 
-containsDuplicate(t3);
+
+val = rotate(...t1);  // leetcode -> unaccepted
+// console.log(val)
+
+// SOLUTION 2
+var rotate2 = function(nums, k) {
+   let j = 0
+   let current, previous;
+
+   for (let j = 0; j < k; j++) {
+      current = nums[nums.length-1];
+      for (let i = 0; i >= nums.length; i--) {
+         previous = nums[0];
+         nums[0] = current;
+
+      }
+      j++;
+   }
+   console.log(nums);
+   return nums;
+};
+
+// rotate2(...t2);  // leetcode -> unaccepted
+
+
+// SOLUTION 3
+// my solution is to swap in this order
+// 1,2,3,4,5,6,7
+// 7,6,5,4,3,2,1
+// 5,6,7,4,3,2,1
+// 5,6,7,1,2,3,4
+
+function reverse(nums, start, end){
+   while (start < end){
+      let hold = nums[start];
+      nums[start] = nums[end];
+      nums[end] = hold;
+      start++;
+      end--;
+   }
+}
+
+var rotate3 = function(nums, k) {
+   if (nums.length < k){
+      reverse(nums, 0, nums.length-1);
+      return nums;
+   }
+   reverse(nums, 0, nums.length-1);
+   reverse(nums, 0, k-1);
+   reverse(nums, k, nums.length-1);
+   
+   return nums;
+}
+
+rotate3(...t1)
