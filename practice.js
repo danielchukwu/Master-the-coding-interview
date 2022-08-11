afda9a06235528e73ed6d378130557e92bd2e758
Daniel Chukwu <00danzy@gmail.com>
2022-08-11 18:41:17 +0100

    complete myhashtable data structure

--- practice.js
+++ practice.js
@@ -15,7 +15,7 @@ class MyHashTable {
    _hash(key){
       let hash = 0;
       for (let i = 0; i < key.length; i++) {
-         hash += key.charCodeAt(i);
+         hash += key.charCodeAt(i) * i;
       }
       hash %= this.data.length;
       return hash;  
@@ -23,18 +23,49 @@ class MyHashTable {
 
    set(key, value){
       const hash = this._hash(key);
-      this.data[hash] = [key, value];
+      if (!this.data[hash]){
+         this.data[hash] = [];
+      }
+      this.data[hash].push([key, value]);
       return this.data;
    }
 
    get(key) {
       const hash = this._hash(key);
-      return this.data[hash][1];
+      if (!this.data[hash]){  // if undefined
+         return undefined;
+      }
+      for (let i = 0; i < this.data[hash].length; i++) {
+         if (this.data[hash][i][0] === key){
+            return this.data[hash][i][1];
+         }
+      }
+   }
+
+   keys() {   // when collision occurs O(n^2) || when no collisions occur O(n)
+      const keysArray = [];
+      for (let i = 0; i < this.data.length; i++) {
+         if (this.data[i] && this.data[i].length > 1) {   // when collisions occur and address has more than one input
+            for (let j = 0; j < this.data[i].length; j++) {
+               keysArray.push(this.data[i][j][0]);
+            }
+         }
+         else if (this.data[i]){                         // when no collision and address only has one input
+            keysArray.push(this.data[i][0][0]);
+         }
+      }
+      console.log(keysArray);
+      return keysArray;
    }
 }
 
 const hashtable = new MyHashTable(50);
-hashtable.set("name", "daniel");
-const name = hashtable.get("name");
+hashtable.set("daniel", 23);
+hashtable.set("naza", 20);
+hashtable.set("uche", 25);
+hashtable.set("stanley", 29);
+
+hashtable.get("stanley")
+hashtable.keys();
 
 hashtable.print()
