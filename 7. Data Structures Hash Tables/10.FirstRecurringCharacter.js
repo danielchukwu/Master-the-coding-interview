888718ebfa46a1560223e6275e49c4275cb5f89f
Daniel Chukwu <00danzy@gmail.com>
2022-08-11 19:23:50 +0100

    complete solution

--- 7. Data Structures Hash Tables/10.FirstRecurringCharacter.js
+++ 7. Data Structures Hash Tables/10.FirstRecurringCharacter.js
@@ -1,6 +1,33 @@
 // Javascript - {}
 // Topic - Google Question (First Recurring Character)
 // Problem: Given an array return the first recurring character
-// Example
-// Input  - 
-// Output - 
+
+// Example 1
+// Input  - [2,5,1,2,3,5,1,2,4]
+// Output - 2
+
+// Example 2
+// Input  - [2,1,1,2,3,5,1,2,4]
+// Output - 1
+
+// Example 3
+// Input  - [2,3,4,5]
+// Output - undefined
+
+function recurringCharacter(arr, target){
+   const object = {};
+   for (let i = 0; i < arr.length; i++) {
+      if (object[arr[i]]){
+         console.log(arr[i], target === arr[i])
+         return arr[i];
+      }
+      object[arr[i]] = true;
+   }
+   return undefined;
+}
+
+const t1 = [[2,5,1,2,3,5,1,2,4], 2];
+const t2 = [[2,1,1,2,3,5,1,2,4], 1];
+const t3 = [[2,3,4,5], undefined];
+
+recurringCharacter(...t1);
--- practice.js
+++ practice.js
@@ -1,71 +1,34 @@
 // Javascript - {}
-// Topic - Build Data Structure
-// Problem: Exercise Implement a Hash Table with the methods below
-//          - set and get
+// Topic - Google Question (First Recurring Character)
+// Problem: Given an array return the first recurring character
 
-class MyHashTable {
-   constructor(size){
-      this.data = new Array(size);
-   }
-   
-   print(){
-      console.log(this.data)
-   }
+// Example 1
+// Input  - [2,5,1,2,3,5,1,2,4]
+// Output - 2
 
-   _hash(key){
-      let hash = 0;
-      for (let i = 0; i < key.length; i++) {
-         hash += key.charCodeAt(i) * i;
-      }
-      hash %= this.data.length;
-      return hash;  
-   }
+// Example 2
+// Input  - [2,1,1,2,3,5,1,2,4]
+// Output - 1
 
-   set(key, value){
-      const hash = this._hash(key);
-      if (!this.data[hash]){
-         this.data[hash] = [];
-      }
-      this.data[hash].push([key, value]);
-      return this.data;
-   }
+// Example 3
+// Input  - [2,3,4,5]
+// Output - undefined
 
-   get(key) {
-      const hash = this._hash(key);
-      if (!this.data[hash]){  // if undefined
-         return undefined;
+function recurringCharacter(arr, target){
+   const object = {};
+   for (let i = 0; i < arr.length; i++) {
+      if (object[arr[i]]){
+         console.log(arr[i], target === arr[i])
+         return arr[i];
       }
-      for (let i = 0; i < this.data[hash].length; i++) {
-         if (this.data[hash][i][0] === key){
-            return this.data[hash][i][1];
-         }
-      }
+      object[arr[i]] = true;
    }
-
-   keys() {   // when collision occurs O(n^2) || when no collisions occur O(n)
-      const keysArray = [];
-      for (let i = 0; i < this.data.length; i++) {
-         if (this.data[i] && this.data[i].length > 1) {   // when collisions occur and address has more than one input
-            for (let j = 0; j < this.data[i].length; j++) {
-               keysArray.push(this.data[i][j][0]);
-            }
-         }
-         else if (this.data[i]){                         // when no collision and address only has one input
-            keysArray.push(this.data[i][0][0]);
-         }
-      }
-      console.log(keysArray);
-      return keysArray;
-   }
+   console.log(undefined)
+   return undefined;
 }
 
-const hashtable = new MyHashTable(50);
-hashtable.set("daniel", 23);
-hashtable.set("naza", 20);
-hashtable.set("uche", 25);
-hashtable.set("stanley", 29);
+const t1 = [[2,5,1,2,3,5,1,2,4], 2];
+const t2 = [[2,1,1,2,3,5,1,2,4], 1];
+const t3 = [[2,3,4,5], undefined];
 
-hashtable.get("stanley")
-hashtable.keys();
-
-hashtable.print()
+recurringCharacter(...t3);
