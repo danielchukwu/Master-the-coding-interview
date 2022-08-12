649c052a616e86ae4bb843316b669d59fcf5ad56
Daniel Chukwu <00danzy@gmail.com>
2022-08-12 14:55:36 +0100

    complete addition of append method

--- practice.js
+++ practice.js
@@ -36,12 +36,18 @@ class LinkedList {
          next: null
       }
       this.tail = this.tail.next;
-      return this.head;
+      this.length++;
+      return this;
    }
+
+   
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.print();
 
 console.log(myLinkedList);
