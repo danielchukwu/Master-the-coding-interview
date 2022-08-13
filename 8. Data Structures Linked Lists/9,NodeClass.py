2efdcb0408e3cc1adcad7717d1789ee2577ec8fa
Daniel Chukwu <00danzy@gmail.com>
2022-08-13 12:13:45 +0100

    complete datas structure

--- 8. Data Structures Linked Lists/9,NodeClass.py
+++ 8. Data Structures Linked Lists/9,NodeClass.py
@@ -7,5 +7,45 @@ class Node:
       self.value = value
       self.next = None
 
-newNode = Node(10)
-print(newNode.next)
+class LinkedList:
+   def __init__(self, value) -> None:
+      self.head = Node(value)
+      self.tail = self.head
+      self.length = 1
+
+   def append(self, value):
+      newNode = Node(value)
+      self.tail.next = newNode
+      self.tail = newNode
+      self.length+=1
+      return self
+
+   def prepend(self, value):
+      newNode = Node(value)
+      newNode.next = self.head
+      self.head = newNode
+      self.length+=1
+      return self
+
+   def printList(self):
+      currentNode = self.head
+      nodesArray = []
+
+      while(currentNode):
+         nodesArray.append(str(currentNode.value))
+         currentNode = currentNode.next
+      
+      nodesArray = "-->".join(nodesArray)
+      print(nodesArray)
+      return self
+
+
+myLinkedList = LinkedList(10)
+myLinkedList.append(5)
+myLinkedList.append(16)
+myLinkedList.prepend(1)
+myLinkedList.prepend(4)
+myLinkedList.append(44)
+myLinkedList.printList()
+
+# print(myLinkedList.)
