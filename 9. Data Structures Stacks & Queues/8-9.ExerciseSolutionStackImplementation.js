613ca196b80d134dc97b9a0760dc5189af8400b1
Daniel Chukwu <00danzy@gmail.com>
2022-08-15 12:13:53 +0100

    complete solution

---
+++ 9. Data Structures Stacks & Queues/8-9.ExerciseSolutionStackImplementation.js
@@ -0,0 +1,75 @@
+// Javascript - {}
+// Topic - Build a Data Structure
+// Problem: Build a Stack Data Structure with the following methods 
+// - peek
+// - push
+// - pop 
+// - isEmpyt (optional)
+
+class Node {
+   constructor(value) {
+      this.value = value;
+      this.next = null;
+   }
+}
+
+class Stack {
+   constructor(value=null){
+      this.top = value ? new Node(value) : null;
+      this.bottom = this.top;
+      this.length = value ? 1 : 0;
+   }
+   peek(){
+      return this.top;
+   }
+   push(value){
+      let newNode = new Node(value);
+      newNode.next = this.top;
+      this.top = newNode;
+      this.length ++;
+      return this;
+   }
+   pop(){
+      // checks: if empty return
+      if (!this.top)  {
+         return null;
+      }
+
+      const newTop = this.top.next;
+      this.top = newTop;
+      this.length--;
+      if (this.top === null) this.bottom = this.top;
+
+      return this;
+   }
+
+   isEmpty(){
+      return !this.length ? true : false;
+   }
+
+   printStack(){
+      let cur = this.top;
+
+      while (cur !== null){
+         console.log(cur.value);
+         console.log("  |  ");
+         cur = cur.next;
+      }
+      console.log("null");
+      return this;
+   }
+}
+
+const myStack = new Stack("google");
+myStack.push('udemy');
+myStack.push('youtube');
+myStack.push('discord');
+myStack.pop();
+myStack.pop();
+myStack.pop();
+myStack.pop();
+
+console.log(myStack.isEmpty())
+
+console.log(myStack.printStack())
+console.log(myStack.length)
