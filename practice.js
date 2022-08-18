bc843c6d15659047c5ec771c61bb123ba47481ce
Daniel Chukwu <00danzy@gmail.com>
2022-08-18 16:37:24 +0100

    complete addEdge

--- practice.js
+++ practice.js
@@ -12,17 +12,29 @@ class Graph {
    }
    addVertex(node){
       // check param: empty return
-      if (node === null || node === undefined) return "missing parameter";
+      if (node === undefined) return "missing parameter";
 
       // check if node already exists: if it doesn't add, else ignore
       if (!this.adjacencyList.hasOwnProperty(node)){   // if node doesn't exist
          this.adjacencyList[node] = [];
+         this.numberOfNodes++;
       } else {                                         // else if node does exist
          return "node already exists in this graph";
       }
    }
-   addEdge(node1, node2){
-      // undirected
+   addEdge(node1, node2){  // undirected
+      // check param: if ether node1 or node2 empty
+      if (node1 === undefined  || node2 === undefined) return "missing parameter"
+
+      const n1 = this.adjacencyList[node1];
+      const n2 = this.adjacencyList[node2];
+
+      if (n1 && n2){
+         n1.push(node2);
+         n2.push(node1);
+      } else {
+         return "incorrect node inputs";
+      }
    }
    showConnections(){
    }
@@ -37,4 +49,14 @@ myGraph.addVertex(4);
 myGraph.addVertex(5);
 myGraph.addVertex(6);
 
+myGraph.addEdge(0, 1);
+myGraph.addEdge(0,2);
+myGraph.addEdge(2,1);
+myGraph.addEdge(2,4);
+myGraph.addEdge(3,1);
+myGraph.addEdge(3,4);
+myGraph.addEdge(4,5);
+myGraph.addEdge(5,6);
+
 console.log(myGraph.adjacencyList)
+console.log(myGraph.numberOfNodes)
