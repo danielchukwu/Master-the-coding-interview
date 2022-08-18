d9569b60272de8c3e2b0ba564b21f53e706d8637
Daniel Chukwu <00danzy@gmail.com>
2022-08-18 15:07:42 +0100

    add problem

--- practice.js
+++ practice.js
@@ -1,76 +1,10 @@
 // Javascript - {}
-// Topic - Coding Interview Problem
-// Problem: Build a Max Heap
-// - insert
+// Topic - Build Data Structure (Graphs)
+// Problem: 7. Build a Graph Data Structure with the following methods 
+// - addVertex
+// - addEdge 
+// - showConnection (tip: method should basically print the entire connection of every vertex in the graph)
 
-class Heap{
-   constructor (isMax=false) {
-      this.array = [];
-      this.isMax = isMax;
-   }
-   insert(value){
-      // check param: data type, nu, exists, is array empty
-      this.array.push(value);
-      if (this.isMax) {this.sortMaxHeap();}
-      else {this.sortMinHeap();}
-
-      return this;
-   }
-
-   sortMaxHeap(){
-      let i = this.array.length;                  // index+1 of last item
-      let j = Math.floor(i/2);  // index+1 of parent item
-      
-      console.log(this.array)
-      console.log(`v=${i} `, `p=${j}`)
-      console.log(`v=${this.array[i-1]} `, `p=${this.array[j-1]}`)
-
-      // sort new value for maximum
-      while (j-1>=0) {
-         let val = this.array[i-1];
-         let parent = this.array[j-1];
-
-         if (val > parent){
-            [this.array[i-1], this.array[j-1]] = [this.array[j-1], this.array[i-1]]  // swapped
-            i = j;
-            j = Math.floor(j/2);
-         } else {
-            break;
-         }
-      }
-   }
-
-   sortMinHeap() {
-      let i = this.array.length; // index+1 for num
-      let j = Math.floor(i/2);   // index+1 for parent
-
-      console.log(this.array)
-      console.log("v=",i, " p=", j);
-      console.log(`${this.array[i-1]} ${this.array[j-1 >= 0? j-1 : 0]}`)
-
-      while(j>=0){
-         let val = this.array[i-1];
-         let parent = this.array[j-1];
-
-         if (val < parent){
-            [this.array[i-1], this.array[j-1]] = [this.array[j-1], this.array[i-1]];
-            i = j;
-            j = Math.floor(j/2);
-         } else {
-            break;
-         }
-      }
-   }
-}
-
-const myHeap = new Heap(false);
-myHeap.insert(5);
-myHeap.insert(12);
-myHeap.insert(64);
-myHeap.insert(1);
-myHeap.insert(37);
-myHeap.insert(90);
-myHeap.insert(91);
-myHeap.insert(9);
-
-console.log(myHeap.array);
+// class Graph {
+   
+// }
