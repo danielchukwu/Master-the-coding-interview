1fe89f976d3e63cb6d25f4b15e0e584f6cd3b48c
Daniel Chukwu <00danzy@gmail.com>
2022-08-19 16:08:45 +0100

    complete solution

--- 12. Algorithms Recursion/5-6.ExerciseFactorial.js
+++ 12. Algorithms Recursion/5-6.ExerciseFactorial.js
@@ -3,18 +3,25 @@
 
 
 function findFactorialRecursive(number) {
-   // code here
+   // base case
+   if (number === 2){
+      return number;
+   }
+
+   // recursive case
+   return findFactorialRecursive(number-1) * number;
 }
 
 function findFactorialIterative(number) {
    // code here
    let result = number;
-   for (let i=number-1; i > 0; i--){
+   for (let i=number-1; i > 1; i--){
       result *= i;
    }
-   return result
+   return result;
 }
 
-// const obj1 = findFactorialRecursive(5);
+const obj1 = findFactorialRecursive(5);
+console.log(`recursive result = ${obj1}`)
 const obj2 = findFactorialIterative(5);
-console.log(obj2);
+console.log(`iteritative result = ${obj2}`)
