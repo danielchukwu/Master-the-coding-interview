0a322a22c46f87a26299420cdcf871840e7d02c7
Daniel Chukwu <00danzy@gmail.com>
2022-08-20 22:29:48 +0100

    complete solution

--- 13. Algorithms BubbleSort/5-6.ExerciseSolutionBubbleSort.js
+++ 13. Algorithms BubbleSort/5-6.ExerciseSolutionBubbleSort.js
@@ -11,7 +11,7 @@
 // console.log(numbers);
 
 
-// Solution 
+// My Solution 
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
@@ -43,3 +43,21 @@ function bubbleSort(array) {  // Bubble sort => Time O(n^2), Space
 
 bubbleSort(numbers);
 console.log(numbers);
+
+
+// Teachers Solution (Better Code than mine)
+function bubbleSort2(array){
+   const length = array.length;
+
+   for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+         if (array[j] > array[j+1]){
+            // swap numbers
+            const temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+         }
+      }
+   }
+}
+
