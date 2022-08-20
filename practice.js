759651c9eb9c93b0fe6703279b1e3577c213b239
Daniel Chukwu <00danzy@gmail.com>
2022-08-20 06:01:19 +0100

    complete iterative solution

--- practice.js
+++ practice.js
@@ -1,27 +1,35 @@
 // Javascript - {}
 // Topic - Exercise Factorial
+// Problem: Given a number N return the index value of thhe Fibonacci sequence, where the sequence is:
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// The pattern of the sequence is that each value is the sum of the 2 previous values, that means N=5 -> 2+3
+
+// function fibonacciIterative(n){
+// //code here
+// }
+// fibonacciIterative(3)
+
+// function fibonacciIterativeRecursive(n){
+// //code here
+// }
+// fibonacciIterativeRecursive(3)
 
 
-function findFactorialRecursive(number) {
-   // base case
-   if (number === 2){
-      return number;
+function fibonacciIterative(n){
+   //check params
+   const fibArray = [0, 1];
+   for (let i = n; i > 1; i--) {
+      fibArray.push(fibArray[fibArray.length-1] + fibArray[fibArray.length-2]);
    }
+   return fibArray[n];
+}
+let result1 = fibonacciIterative(3);
+console.log(result1);
+
+function fibonacciIterativeRecursive(n){
+   // base case
 
    // recursive case
-   return findFactorialRecursive(number-1) * number;
 }
+fibonacciIterativeRecursive(3)
 
-function findFactorialIterative(number) {
-   // code here
-   let result = number;
-   for (let i=number-1; i > 1; i--){
-      result *= i;
-   }
-   return result;
-}
-
-const obj1 = findFactorialRecursive(5);
-console.log(`recursive result = ${obj1}`)
-const obj2 = findFactorialIterative(5);
-console.log(`iteritative result = ${obj2}`)
