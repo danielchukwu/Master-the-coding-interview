b244578c2e5bb1485956423664cb79fbe8a1e177
Daniel Chukwu <00danzy@gmail.com>
2022-08-22 06:21:29 +0100

    complete solution

--- practice.js
+++ practice.js
@@ -27,7 +27,7 @@ function insertionSort(array){
       for (let i=cursor; i > 0; i--){
          // compare current with 
          if (array[i] < array[i-1]){
-            // swap smaller number with 
+            // swap smaller number with
             // greater number
             let temp = array[i];
             array[i] = array[i-1];
@@ -38,8 +38,32 @@ function insertionSort(array){
          }
       }
    }
-
 }
 
-insertionSort(numbers);
-console.log(numbers);
+// insertionSort(numbers);
+// console.log(numbers);
+
+
+
+// Tutors Solution
+
+function insertionSort2(array){
+   for (let i = 0; i < array.length; i++) {
+      if (array[i] < array[0]){
+         // move number to the first position
+         array.unshift(array.splice(i,1)[0]);
+         
+      } else {
+         // find where number should go
+         for (let j = 1; j < i; j++) {
+            if(array[i] > array[j-1] && array[i] < array[j]){
+               // move number to the right position
+               array.splice(j, 0, array.splice(i,1)[0]);
+            }
+         }
+      }
+   }
+}
+
+insertionSort2(numbers);
+console.log(insertionSort2);
